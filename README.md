673feb2bf963a30f592f1cd0b7ee52d8352347d2
justicedong <justicedong@naver.com>
2022-01-10 11:48:24 +0900

    Add: Linear Regression

---
+++ README.md
@@ -0,0 +1,7 @@
+# Goal 
+
+This is a my deep learning study record.
+
+It only include code.
+
+If you want get some summary, then go to [here.](https://perfect-dahlia-9e0.notion.site/Deep-Learning-2bd8f420af1a45a18c39d32a09020820)
---
+++ pytorch/LinearRegression/main.py
@@ -0,0 +1,59 @@
+import torch
+from torch import nn
+
+torch.manual_seed(1)
+
+
+class Data(torch.utils.data.Dataset):
+    """Some Information about Data"""
+
+    def __init__(self):
+        self.x = torch.zeros(20, 2)
+        self.x[:, 0] = torch.arange(-1, 1, 0.1)
+        self.x[:, 1] = torch.arange(-1, 1, 0.1)
+        self.w = torch.tensor([[1.0, -1.0], [1.0, 3.0]])
+        self.b = torch.tensor([[-1.0, 1.0]])
+        self.f = torch.mm(self.x, self.w) + self.b
+
+        # make random noize
+        self.y = self.f + 0.001 * torch.randn(self.x.shape[0], 1)
+        self.len = self.x.shape[0]
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+
+    def __len__(self):
+        return self.len
+
+class LinearRegression(nn.Module):
+  """Some Information about LinearRegression"""
+  def __init__(self, input_size, output_size):
+    super(LinearRegression, self).__init__()
+    self.linear = nn.Linear(input_size, output_size)
+
+  def forward(self, x):
+    yhat = self.linear(x)
+    return yhat
+
+model = LinearRegression(2, 2)
+
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+criterion = nn.MSELoss()
+
+dataset = Data()
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=5)
+
+LOSS = []
+epochs = 100
+
+for epoch in range(epochs):
+  for x, y in dataloader:
+    yhat = model(x)
+    loss = criterion(yhat, y)
+    LOSS.append(loss.item())
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+print(LOSS)
